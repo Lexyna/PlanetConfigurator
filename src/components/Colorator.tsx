df43c1afe178e3209db1ffc89138d0d491e6bf2c
Lexyna <lexieverena@outlook.de>
2022-02-17 08:57:03 +0100

    Added primitive implementation of colorPicker html elemnts

--- src/components/Colorator.tsx
+++ src/components/Colorator.tsx
@@ -1,15 +1,53 @@
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
-import { ChromePicker } from "react-color";
+import { SketchPicker } from "react-color";
 
 export const Colorator = () => {
 
+    const [pickers, setPickers] = useState([
+        { id: nanoid() },
+        { id: nanoid() },
+        { id: nanoid() }
+    ]);
+
+    const addPicker = () => {
+        const picker = { id: nanoid() };
+        setPickers(pickers.concat(picker));
+    }
+
+    return (
+        <div style={{ display: "inline" }}>
+            <p>Color Settings:</p>
+            {pickers.map(picker => {
+                return <PlanetColorPicker key={picker.id} />
+            })}
+            <button onClick={addPicker}>Add</button>
+        </div>
+    )
+}
+
+const PlanetColorPickers = (length: number) => {
+
+    const pickers: any[] = [];
+    let id: number = 0;
+
+    for (var i = 0; i < length; i++) {
+        pickers.push(PlanetColorPicker())
+    }
+    return pickers;
+}
+
+const PlanetColorPicker = () => {
+
     const [display, setDisplay] = useState(false);
+
     const [color, setColor] = useState({
         r: 241,
         g: 112,
         b: 19,
         a: 1
     });
+
     const onClickMethod = () => {
         setDisplay(!display);
     }
@@ -66,28 +104,36 @@ export const Colorator = () => {
     } as React.CSSProperties
 
     return (
-        <div style={{ display: "inline" }}>
-            <p>Color Settings:</p>
-            <div style={{ display: "flex", border: "1px solid black", width: "inherit" }}>
-                <div style={inputDivStyle}>
-                    <input
-                        type="number"
-                        defaultValue={0}
-                        min={0}
-                        max={1}
-                        step={0.01}
-                        style={inputStyle}
-                    />
+        <div style={{ display: "flex", border: "1px solid black", width: "inherit" }}>
+            <div style={inputDivStyle}>
+                <input
+                    type="number"
+                    defaultValue={0}
+                    min={0}
+                    max={1}
+                    step={0.01}
+                    style={inputStyle}
+                />
+            </div>
+            <div>
+                <div style={swatch} onClick={onClickMethod}>
+                    <label>Color: </label> <div style={colorStyle} />
                 </div>
-                <div>
-                    <div style={swatch} onClick={onClickMethod}>
-                        <label>Color: </label> <div style={colorStyle} />
-                    </div>
-                    {display ? <div style={popover}>
-                        <div style={cover} onClick={onCloseMethod} />
-                        <ChromePicker color={color} onChange={onChangeMethod} />
-                    </div> : null}</div>
+                {display ? <div style={popover}>
+                    <div style={cover} onClick={onCloseMethod} />
+                    <SketchPicker color={color} onChange={onChangeMethod} />
+                </div> : null}
             </div>
         </div>
-    )
+    );
 }
+
+/**
+ * <div style={swatch} onClick={onClickMethod}>
+                    <label>Color: </label> <div style={colorStyle} />
+                </div>
+                {display ? <div style={popover}>
+                    <div style={cover} onClick={onCloseMethod} />
+                    <ChromePicker color={color} onChange={onChangeMethod} />
+                </div> : null}
+ */
