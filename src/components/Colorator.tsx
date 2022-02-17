e96aa7cdcf0be6821424e8119653aa1e327d5a73
Lexyna <lexieverena@outlook.de>
2022-02-17 16:38:41 +0100

    Implemeted dynamic handling of ColorMappings

--- src/components/Colorator.tsx
+++ src/components/Colorator.tsx
@@ -1,13 +1,34 @@
+import { nanoid } from "nanoid";
 import React, { useState } from "react";
 import { ColorResult, SketchPicker } from "react-color";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { planetActionCreators } from "../store";
 import { colorMappingIdSelector, colorMappingSelector } from "../store/selectors/planetSelector";
+import { ColorMapping } from "../types/planetProp";
 
 export const Colorator = () => {
 
     const colorMappings = useSelector(colorMappingSelector);
+
+    const dispatch = useDispatch();
+
+    const { addColorMapping } = bindActionCreators(
+        planetActionCreators,
+        dispatch
+    )
+
+    const defaultColorMapping: ColorMapping = {
+        id: "none",
+        value: 0,
+        color: { r: 0, g: 0, b: 0, a: 1 }
+    }
+
+    const onAddColorMapping = () => {
+        const uniqueId = nanoid();
+        defaultColorMapping.id = uniqueId;
+        addColorMapping(defaultColorMapping);
+    }
 
     return (
         <div style={{ display: "inline" }}>
@@ -15,7 +36,7 @@ export const Colorator = () => {
             {colorMappings.map(mappings => {
                 return <PlanetColorPicker id={mappings.id} key={mappings.id} />
             })}
-            <button>Add</button>
+            <button onClick={onAddColorMapping}>Add</button>
         </div>
     )
 }
@@ -32,19 +53,21 @@ const PlanetColorPicker: React.FC<{ id: string }> = (props) => {
 
     const dispatch = useDispatch();
 
-    const { updateColorMapping } = bindActionCreators(
+    const { updateColorMapping, removeColorMapping } = bindActionCreators(
         planetActionCreators,
         dispatch
     );
 
-    console.log("picker1: " + props.id)
-    console.log("Mapping: ")
-    console.log(mappingColor)
-
-    const onChangeMethod = (color: ColorResult) => {
+    const onChangeColorMethod = (color: ColorResult) => {
         mappingColor.color = color.rgb;
         updateColorMapping(mappingColor);
         setColor({ ...color.rgb })
+    }
+
+    const onChangeValue = (value: number) => {
+        mappingColor.value = value;
+        updateColorMapping(mappingColor);
+        setValue(value)
     }
 
     const onClickMethod = () => {
@@ -55,11 +78,15 @@ const PlanetColorPicker: React.FC<{ id: string }> = (props) => {
         setDisplay(false);
     }
 
+    const onRemoveColorMapping = (id: string) => {
+        removeColorMapping(id);
+    }
+
     const colorStyle = {
         marginTop: "3px",
-        marginLeft: "3px",
+        marginLeft: "7px",
         padding: "0px",
-        width: "36px",
+        width: "45px",
         height: "18px",
         borderRadius: "2px",
         background: `rgb(${color.r}, ${color.g}, ${color.b}, ${color.a})`,
@@ -99,7 +126,7 @@ const PlanetColorPicker: React.FC<{ id: string }> = (props) => {
     } as React.CSSProperties
 
     return (
-        <div style={{ display: "flex", border: "1px solid black", width: "inherit" }}>
+        <div style={{ display: "flex", border: "1px solid black", width: "inherit", paddingInline: "5px", paddingTop: "2px", marginTop: "3px", paddingBottom: "2px" }}>
             <div style={inputDivStyle}>
                 <input
                     type="number"
@@ -108,6 +135,8 @@ const PlanetColorPicker: React.FC<{ id: string }> = (props) => {
                     max={1}
                     step={0.01}
                     style={inputStyle}
+                    onChange={
+                        ({ target: { value } }) => onChangeValue(parseFloat(value))}
                 />
             </div>
             <div>
@@ -116,26 +145,12 @@ const PlanetColorPicker: React.FC<{ id: string }> = (props) => {
                 </div>
                 {display ? <div style={popover}>
                     <div style={cover} onClick={onCloseMethod} />
-                    <SketchPicker color={color} onChange={onChangeMethod} />
+                    <SketchPicker color={color} onChange={onChangeColorMethod} />
                 </div> : null}
+            </div>
+            <div>
+                <button style={{ marginLeft: "0%", marginTop: "30%" }} onClick={() => onRemoveColorMapping(props.id)}>X</button>
             </div>
         </div>
     );
 }
-
-/**
- * <div style={swatch} onClick={onClickMethod}>
-                    <label>Color: </label> <div style={colorStyle} />
-                </div>
-                {display ? <div style={popover}>
-                    <div style={cover} onClick={onCloseMethod} />
-                    <ChromePicker color={color} onChange={onChangeMethod} />
-                </div> : null}
-                mappingColor.color = val.rgb;
-                        mappingColor.color.alpha = val.rgb.a;
-                        if (mappingColor.color.alpha) {
-                            mappingColor.color.alpha *= 255;
-                            console.log(mappingColor.color.alpha)
-                        }
-                        updateColorMapping(mappingColor);
- */
