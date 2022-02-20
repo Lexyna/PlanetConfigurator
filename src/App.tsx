11455bf8a8b28651bf9cf52f044c885b44e920f9
Lexyna <lexieverena@outlook.de>
2022-02-20 15:13:36 +0100

    Incuded tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Keyframe } from './components/Keyframe';
-import { Setting } from './components/Settings';
 
 function App() {
   return (
     <div>
-      <div className="editor">
-        <header>PlanetProperties</header>
-        < Setting />
-      </div>
-      <Keyframe />
     </div>
   );
 }
 
 export default App;
+
+/**
+ * /*<div className="editor">
+        <header>PlanetProperties</header>
+        < Setting />
+      </div>
+      <Keyframe />/*
+
+ */
