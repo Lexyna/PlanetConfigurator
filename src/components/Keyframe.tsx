4e317858e4d9b958e2a6b40e091540ad2cb95df2
Lexyna <lexieverena@outlook.de>
2022-03-28 16:44:45 +0200

    Implemented Play/Pause button for Keyframe

--- src/components/Keyframe.tsx
+++ src/components/Keyframe.tsx
@@ -3,11 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
 import { Animator } from "../Logic/renderer/Animator";
 import { renderSettingsCreator } from "../store";
-import { keyframeSelector } from "../store/selectors/renderSelector";
+import { animateSelector, keyframeSelector } from "../store/selectors/renderSelector";
+import { IoStopCircle, IoPlay } from "react-icons/io5";
 
 export const Keyframe = () => {
 
     const dispatch = useDispatch();
+
+    const animate = useSelector(animateSelector);
 
     const frame = useSelector(keyframeSelector);
 
@@ -17,10 +20,14 @@ export const Keyframe = () => {
     );
 
     return (
-        <div className="absolute bottom-5 left-20 right-20">
+        <div className="absolute bottom-5 left-20 right-20 text-gray-500 flex">
+            {animate ?
+                <IoStopCircle className="pb-0" size="30" onClick={() => updateRenderSettingAnimate(false)} /> :
+                <IoPlay className="pb-0" size="30" onClick={() => updateRenderSettingAnimate(true)} />
+            }
             <input
-                className="w-full bg-gray-600 rounded-xl h-6 p-0
-                focus:outline-none focus:ring-0 focus:shadow-none appearance-none"
+                className="w-full bg-gray-600 rounded-xl h-6 p-0 mt-1
+                focus:outline-none focus:ring-0 focus:shadow-none appearance-none ml-2"
                 type="range"
                 min={0}
                 max={255}
